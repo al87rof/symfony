258e21b8624ebe0207d2f6386c487edeebed2b63
al87rof <alexandr_r@i.ua>
2016-09-24 12:18:36 +0300

    firs commit

---
+++ src/A2x/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace A2x\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('A2xBlogBundle:Default:index.html.twig');
+    }
+}
